9d9bfefb0e43130d680b3c227a96b90b61892c71
n01nex <n01nex@proton.me>
2025-07-23 13:46:29 +0200

    first structure of api query with user input with file execution in sys

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,8 +9,22 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    print("Hello from agent-exp!")
 
+    if len(sys.argv) < 2:
+        raise Exception("No Prompt provided")
+
+    else:
+        print("Hello from agent-exp!")
+
+        model = "gemini-2.0-flash-001"
+        prompt = sys.argv[1]
+
+        response = client.models.generate_content(model=model, contents=prompt)
+        prt_tokens = response.usage_metadata.prompt_token_count
+        rsp_tokens = response.usage_metadata.candidates_token_count
+        print(response.text)
+        print(f"Prompt tokens: {prt_tokens}")
+        print(f"Response tokens: {rsp_tokens}")
 
 if __name__ == "__main__":
     main()
