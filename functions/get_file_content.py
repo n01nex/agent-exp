68f568782e0989fa544711f67cfebb5b1f4ce8a2
n01nex <n01nex@proton.me>
2025-07-25 15:49:30 +0200

    completed get_file_content and relevant tests

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,10 +1,13 @@
 import os
+from . import config
+
+MAX_CHARS = config.MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     full_path = os.path.join(working_directory, file_path)
     absolute_path = os.path.abspath(full_path)
     absolute_wd = os.path.abspath(working_directory)
-    MAX_CHARS = 10000
+ 
     
     # File Path Validity checks
     if not absolute_path.startswith(absolute_wd):
@@ -14,6 +17,15 @@ def get_file_content(working_directory, file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     
     # read file in string
-    with open(file_path, "r") as f:
-        file_content_string = f.read(MAX_CHARS)
-    
+    try:
+        with open(absolute_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if os.path.getsize(absolute_path) > MAX_CHARS:
+                file_content_string += f"[...File {file_path} truncated at {MAX_CHARS} characters]"
+            
+            return file_content_string
+
+    except Exception as e:
+        return f"Error: {e}"
+
+
